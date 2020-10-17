81f0793a52e5c4c91114d22b78ed33d9ad6473c9
nstammewar <nstammewar@gmail.com>
2020-10-18 03:12:22 +0530

    enabled eureka in userServices

--- microservice-user-management/src/main/java/com/nil/microserviceusermanagement/controller/UserControllerImpl.java
+++ microservice-user-management/src/main/java/com/nil/microserviceusermanagement/controller/UserControllerImpl.java
@@ -4,6 +4,9 @@ import com.nil.microserviceusermanagement.model.Role;
 import com.nil.microserviceusermanagement.model.User;
 import com.nil.microserviceusermanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,17 +20,39 @@ import java.util.List;
 @Controller
 public class UserControllerImpl {
     @Autowired
+    private DiscoveryClient discoveryClient;
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.application.name}")
+    private String serviceId;
+
+    @GetMapping("/service/port")
+    public String getPort(){
+        return "Service port number : " + env.getProperty("local.server.port");
+    }
+
+    @GetMapping("/service/instances")
+    public ResponseEntity<?> getInstances(){
+        return new ResponseEntity<>(discoveryClient.getInstances(serviceId), HttpStatus.OK);
+    }
+    @GetMapping("/service/services")
+    public ResponseEntity<?> getServices(){
+        return new ResponseEntity<>(discoveryClient.getServices(), HttpStatus.OK);
+    }
 
     @PostMapping("/service/registration")
     public ResponseEntity<?> saveUser(@RequestBody User user){
         if(userService.findByUsername(user.getUsername())!= null){
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
-
         user.setRole(Role.USER);
         return new ResponseEntity<>(userService.save(user),HttpStatus.CREATED);
     }
+
     @GetMapping("/service/login")
     public ResponseEntity<?> getUser(Principal principal){
         //Principal principal = request.getUserPrincipal();
@@ -39,6 +64,7 @@ public class UserControllerImpl {
         );
 
     }
+
     @PostMapping("/service/name")
     public ResponseEntity<?> getNameOfUsers(@RequestBody List<Long>idList){
         return ResponseEntity.ok(userService.findUsers(idList));
--- microservice-user-management/src/main/java/com/nil/microserviceusermanagement/repository/UserRepository.java
+++ microservice-user-management/src/main/java/com/nil/microserviceusermanagement/repository/UserRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 
-    @Query("select u.name from User u where u.id in(:pIdList")
+    @Query("select u.name from User u where u.id in(:pIdList)")
     List<String> findByIdList(@Param("pIdList") List<Long> idList);
 
 
